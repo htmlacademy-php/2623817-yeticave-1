61cdeb276733b5dfbc9daf123ad488987a5ce661
Maiseyenko.M <maiseyenko.m@gmail.com>
2025-02-24 15:07:24 +0300

    MODULE3_TASK1: templates

---
+++ dataFormatting.php
@@ -0,0 +1,19 @@
+<?php
+function getValueInMoneyType (float $value): string {
+
+    //Получаем целую часть числа
+    $intValue = ceil($value);
+    $result = (string) $intValue;
+
+    //Добавляем группировку цифр, если надо
+    if ($intValue >= 1000) {
+        $result = number_format($intValue,0,".",' ');   
+    };
+
+    //Добавляем знак рубля
+    $result .= " ₽";
+
+    return $result;
+}
+
+?>
