f9932c8412245f87598b3b1630c6a33b9418cafb
Maiseyenko.M <maiseyenko.m@gmail.com>
2025-03-03 21:12:09 +0300

    module5-task1: working with db

---
+++ db/DBConfig.php
@@ -0,0 +1,9 @@
+<?PHP 
+    define("DB_CONFIG" , [
+        'hostname' => '127.127.126.26',
+        'login' => "root",
+        'password' => "",
+        'database' =>"yeticave_1"
+    ]);
+
+?>
---
+++ db/DBQueries.php
@@ -0,0 +1,34 @@
+<?php 
+
+define("DB_QUERIES" , [
+    
+    //Получение списка категорий
+    'getCategoryList' => "SELECT categories.id as id, categories.name as name 
+        FROM yeticave_1.categories as categories;",
+    
+    //Получение списка открытых лотов. 
+    //Считаем, что лот открыт, если нет победителя
+    //Считаем, что цену = максимальная ставка. если ставок нет цена = первоначальная цена
+    'getItemList' => "SELECT 
+        lots.id as lot_id,
+        lots.name as lot_name,
+        lots.start_price as start_price,
+        lots.image_path as image_path,
+        categories.name as category,
+        lots.expiration_date as expiration_date,
+        IFNULL(prices.price,lots.start_price) as price -- можно сделать 'prices.price + lots.price_step', если нужна последняя ставка + шаг
+    FROM yeticave_1.lots as lots
+        LEFT JOIN yeticave_1.categories as categories ON
+            lots.category_id = categories.id
+        LEFT JOIN (SELECT
+            bets.lot_id as lot_id,
+            max(bets.price) as price
+            FROM yeticave_1.bets as bets
+            GROUP BY bets.lot_id) as prices ON 
+            lots.id = prices.lot_id
+    WHERE
+        lots.winner_id IS NULL 
+    order by created_at DESC"
+]);
+
+?>
