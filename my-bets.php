65353b9efeaa3963f04ff3bf8eff3762bf3e64e5
Maiseyenko.M <maiseyenko.m@gmail.com>
2025-04-22 15:12:50 +0300

    refactoring my-bets page

--- my-bets.php
+++ my-bets.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once('helpers.php');
 require_once('db/DBFunctions.php');
 require_once('layout.php');
@@ -9,35 +10,33 @@ if (session_status() != PHP_SESSION_ACTIVE) {
 if (!isset($_SESSION['id'])) {
         http_response_code(403);
        exit();
-   }
+}
 //Получение данных страницы
 $mysqlConnection = db_get_connection();
-if (!$mysqlConnection){
+if (!$mysqlConnection) {
     http_response_code(500); // переделано, чтобы возвращало ошибку
     exit();
 }
 
 //Список категорий{
 $dbCategoryList = db_get_category_list($mysqlConnection);
-$categoryList = array_column($dbCategoryList,'name','id');
+$categoryList = array_column($dbCategoryList, 'name', 'id');
 //}Список категорий
 
 $queryParam = ['user_id' => $_SESSION['id']];
-$paramCategory = filter_input(INPUT_GET,'category',FILTER_UNSAFE_RAW);
-if($paramCategory != null){ 
+$paramCategory = filter_input(INPUT_GET, 'category', FILTER_UNSAFE_RAW);
+if ($paramCategory != null) {
     //Проверить, есть ли такая категория
-    if(isset($categoryList[$paramCategory])){
+    if (isset($categoryList[$paramCategory])) {
         $queryParam['category'] = $paramCategory;
-    }
-    else{ 
-        http_response_code(404);  
+    } else {
+        http_response_code(404);
         exit();
     }
-    
 }
 
 //Список ставок{
-$dbMyBetsList = db_get_bets_by_user($mysqlConnection,$queryParam);
+$dbMyBetsList = db_get_bets_by_user($mysqlConnection, $queryParam);
 //}Список ставок
 
 db_close_connection($mysqlConnection);
@@ -48,14 +47,10 @@ $myBetsData = [
     'categoryList' => $categoryList,
     'myBetsList' => $dbMyBetsList
 ];
-$myBetPageHTML = include_template('my-bets.php',$myBetsData);
+$myBetPageHTML = include_template('my-bets.php', $myBetsData);
 
 
 //подготовка блока layout
-$mainPageHTML = get_layout_html('Мои ставки',$myBetPageHTML);
+$mainPageHTML = get_layout_html('Мои ставки', $myBetPageHTML);
 
-Print($mainPageHTML);
-?>
-
-
-
+print($mainPageHTML);
